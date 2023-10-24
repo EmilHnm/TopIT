7563c27d6464006ae15641903e2cf201f904003c
manhnguyen2906 <manhnguyenk129@gmail.com>
2023-10-24 14:43:55 +0700

    Tao file hello

--- app/Models/CVSave.php
+++ app/Models/CVSave.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+// anh hoa hoi gay
 class CVSave extends Model
 {
     use HasFactory;
